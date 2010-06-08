843898dd9a9275dbf3fd08d4a76435fc746245bf
Lars Gregori <lars.gregori@gmail.com>
2010-06-08 22:47:40 +0200

    using clojure 1.2; clojure-hadoop 1.2.1 (hadoop 0.20.2)

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject haddit "0.1"
+(defproject haddit "0.1.0"
   :description      "Feeding Reddit to Hadoop"
   :url              "http://github.com/choas/haddit"
   :namespaces       [haddit]
-  :dependencies     [[org.clojure/clojure "1.1.0"]
-		     [org.clojars.choas/clojure-hadoop "1.2.0-SNAPSHOT"]])
+  :dependencies     [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+		     [org.clojars.choas/clojure-hadoop "1.2.1-SNAPSHOT"]])
